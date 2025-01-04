ed13241155ba959382d3735a6a844e368aa24bb3
B-litljon <brandonc.littlejohn@gmail.com>
2025-01-03 16:22:53 -0800

    add abstract Strategy class for defining trading strategies with methods for analysis and order parameters

--- src/strategies/strategy.py
+++ src/strategies/strategy.py
@@ -1,2 +1,37 @@
-# defines a class representing parameters for a genaric 'strategy' object
-# this class will be versitile so that any parameters for a strategy can be passed in
+from abc import ABC, abstractmethod
+from typing import List
+from core.signal import Signal
+from utils.order_params import OrderParams
+
+class Strategy(ABC):
+    """
+    Abstract base class for trading strategies.
+
+    Methods:
+        analyze(self, data): Analyzes market data and returns a list of Signals.
+        get_order_params(self): Returns default OrderParams for the strategy.
+    """
+
+    @abstractmethod
+    def analyze(self, data) -> List[Signal]:
+        """
+        Analyzes market data and returns a list of trading signals.
+
+        Args:
+            data: Market data (format depends on your DataHandler).
+
+        Returns:
+            List[Signal]: A list of trading signals.
+        """
+        pass
+
+    @abstractmethod
+    def get_order_params(self) -> OrderParams:
+        """
+        Returns the default OrderParams for this strategy.
+
+        Returns:
+            OrderParams: An OrderParams object.
+        """
+        pass
+
